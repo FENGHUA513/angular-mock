bc356516a2f78ab1675bafea7aa9f29740959345
xiong <597709174@qq.com>
2019-03-04 17:47:41 +0800

    update code

--- src/app/interface/list.ts
+++
@@ -1,4 +0,0 @@
-export interface List {
-    code: boolean;
-    list: Array<number>;
-}
--- src/app/main/page3/page3.component.ts
+++ src/app/main/page3/page3.component.ts
@@ -1,5 +1,4 @@
 import { ApiService } from './../../servers/api.service';
-import { List } from './../../interface/list';
 import { map, switchMap, concatMap, tap, concatAll } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { fromEvent, interval, Observable, from, of } from 'rxjs';
@@ -13,17 +12,13 @@ export class Page3Component implements OnInit {
   constructor(private http: HttpClient, private apiService: ApiService) { }
   private list;
   ngOnInit() {
-    // const params = new HttpParams().set('fff', '3').set('ddd', 'fdsffdsafs');
+  }
+  getdata() {
     const params = new HttpParams({fromString: 'orderBy="$key"&limitToFirst=1'});
-    this.apiService.get('/api/list', params).subscribe(
-      res => this.list = res.list
+    this.apiService.get('/api/list', params).pipe(
+      map(res => res.list)
+    ).subscribe(
+      res => this.list = res
     );
   }
-  // getlist() {
-  //   return this.apiService.get('/api/list').pipe(map(res => res.list));
-  // }
-  // getdata() {
-  //   const clicks = fromEvent(document.querySelector('#btn'), 'click');
-  //   const result = clicks.concatAll(from(this.getlist()));
-  // }
 }
