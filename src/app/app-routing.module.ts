8775290b5bd0bc47949f87f808f01d81a4bb5597
xiong <597709174@qq.com>
2019-08-16 17:10:47 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { Page1Component } from './main/page1/page1.component';
-import { Page2Component } from './main/page2/page2.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
 import { PreloadService } from './provieders/preloadStrategy';
 const routes: Routes = [
   { path: 'page1/:id', component: Page1Component },
   { path: 'page2', component: Page2Component },
-  { path: 'page3', loadChildren: () => import('./main/page3/page3.module').then(m => m.Page3Module), data: { preload: true } },
+  { path: 'page3', loadChildren: () => import('./pages/page3/page3.module').then(m => m.Page3Module), data: { preload: true } },
   { path: '', redirectTo: '/page2', pathMatch: 'full' }
 ];
 
